8ddcd026736bd0514ea897a81849034244ac40ea
Viki7sh <62552986+Viki7sh@users.noreply.github.com>
2020-04-23 22:19:33 +0300

    Add files via upload

---
+++ Lab 2/2_1/Task 1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Лаба_2__1_
+{
+    class Task_1
+    {
+
+        public static void Main(string[] args)
+        {
+            for (int a = 1; a <= 12; a++)
+            {
+                DateTime dateTime = new DateTime(1, a, 1);
+                Console.WriteLine(dateTime.ToString("MMMM", CultureInfo.GetCultureInfo("fr-FR")));
+            }
+        }
+    }
+}
---
+++ Lab 2/2_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Лаба_2__2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            int letter;
+            string res;
+
+            for (int i=1; i<=4; i++)
+            {
+                letter = rnd.Next(0, 26);
+                res = String.Concat(letters[letter]);
+                Console.Write(res);
+            }
+        }
+    }
+}
