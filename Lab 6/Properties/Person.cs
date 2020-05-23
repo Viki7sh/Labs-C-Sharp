eb8a944401ee53fae57173d8bb2d82d5994391a0
Viki7sh <62552986+Viki7sh@users.noreply.github.com>
2020-05-23 16:42:38 +0300

    Add files via upload

---
+++ Lab 6/Properties/Person.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+
+
+
+namespace Lab_6.Properties
+{
+    public enum Sex
+    {
+        Female,
+        Male
+    }
+
+    internal interface IPerson
+    {
+        Sex GetSexInfo();
+        bool ArmyOrNot();
+
+        string GetYearInfo();
+        void PrintInfo();
+        string GetFormOfStudyInfo();
+
+    }
+    public abstract class Person : IPerson, IComparable
+    {
+        public readonly int _age;
+        private int _personId;
+        protected double Height;
+        public readonly string Name;
+        protected readonly string LastName;
+        public int CompareTo(object o)
+        {
+            Person p = o as Person;
+            if (p != null)
+                return this.LastName.CompareTo(p.LastName);
+            else
+                throw new Exception("Невозможно сравнить два объекта");
+        }
+        private static int PersonCount;
+        protected Sex sex;
+        public Sex GetSexInfo()
+        {
+            return sex;
+        }
+
+        protected Person()
+        {
+        }
+
+        protected Person(int age, double height, string name, string lastName, Sex sex)
+        {
+            this._age = age;
+            this.Height = height;
+            this.Name = name;
+            this.LastName = lastName;
+            _personId = ++PersonCount;
+
+            this.sex = sex;
+        }
+
+        public virtual bool ArmyOrNot()
+        {
+            return sex == Sex.Male && _age < 27 && _age >= 18;
+        }
+
+        public string GetYearInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFormOfStudyInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Lab 6/Properties/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Mime;
+using System.Text;
+
+namespace TestSolution.Properties
+{
+    public class Student : Person, IPerson
+    {
+        private readonly StudentInfo _info;
+
+        protected Student(int age, double height, string name, string lastName, string university, int year,
+            bool formOfStudy, Sex sex) : base(
+            age, height, name, lastName, sex)
+        {
+            _info.University = university;
+            _info.Year = year;
+            _info.FormOfStudy = formOfStudy;
+        }
+
+        private struct StudentInfo
+        {
+            public string University;
+            public int Year;
+            public bool FormOfStudy;
+        }
+
+        public override bool ArmyOrNot()
+        {
+            if (base.ArmyOrNot())
+            {
+                if (_info.Year != 0)
+                    return false;
+                return true;
+            }
+
+            return false;
+        }
+
+        protected virtual string GetYearInfo()
+        {
+            if (_info.Year == 0)
+            {
+                return " вовсе не студент.";
+            }
+
+            //return $"учится на {Info.year} курсе в {Info.university}";
+
+            return _info.Year == 0 ? " вовсе не студент" : $"учится на {_info.Year} курсе в {_info.University}"; //- Почему не работает?
+        }
+
+        protected string GetFormOfStudyInfo()
+        {
+            if (_info.FormOfStudy)
+                return " Платная.";
+            else
+                return " Бюджетная.";
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Студент {LastName} {Name} " + GetYearInfo() + $" ростом {Height}. \nПол: {sex}" + "\nФорма обучения: " + GetFormOfStudyInfo());
+        }
+    }
+}
